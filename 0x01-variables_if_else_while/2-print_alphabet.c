5a112641fe5fa975fba442984f70c37953bfdac3
Sandybee1 <sandraboateng05@gmail.com>
2023-04-22 04:34:21 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	int n = 97;
+
+	do {
+		putchar(n);
+		n++;
+	} while (n <= 122);
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: 0 (Success)
+  */
+int main(void)
+
+{
+	int n = 97;
+
+	do {
+		putchar(n);
+		n++;
+	} while (n <= 122);
+int a = 65;
+do {
+	putchar(a);
+	a++;
+} while (a <= 90);
+putchar('\n');
+return (0);
+}
